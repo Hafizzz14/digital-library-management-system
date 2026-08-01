4abd3247ebdd893495961cd1b0946f39725088c1
Muhamad Hafiz <moch07hafidz.com>
2026-08-01 21:44:19 +0700

    fix: configure allowed hosts for deployment

--- core/settings.py
+++ core/settings.py
@@ -29,10 +29,11 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = os.getenv(
-    "ALLOWED_HOSTS",
-    "127.0.0.1,localhost"
-).split(",")
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+    "hfvzzz14.pythonanywhere.com",
+]
 
 
 # Application definition
